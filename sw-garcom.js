0470d23aedf9b3dc3a86f6fd3968a75da0ebee74
loreny17 <danielloreny2@gmail.com>
2026-05-05 02:35:36 -0300

    Add files via upload

---
+++ sw-garcom.js
@@ -0,0 +1,40 @@
+// Service Worker do Bávaro Garçom
+// Versão simples: cache só do app shell, mas sempre busca do Vercel as atualizações
+const CACHE = 'bavaro-garcom-v1';
+const URLS = [
+  '/garcom.html',
+  '/icon-garcom-192.png',
+  '/icon-garcom-512.png',
+  '/manifest-garcom.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(URLS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network first - sempre tenta a rede, cai no cache se offline
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        // Atualiza o cache com a resposta nova
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
